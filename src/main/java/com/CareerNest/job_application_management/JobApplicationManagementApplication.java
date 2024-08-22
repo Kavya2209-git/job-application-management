cf5d0fc5b12a3b061002f3e1d6709d93a7870c50
2020wa86064@wilp.bits-pilani.ac.in <2020wa86064@wilp.bits-pilani.ac.in>
2024-08-22 15:30:07 +0530

    Initial commit - Project setup

---
+++ src/main/java/com/CareerNest/job_application_management/JobApplicationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.CareerNest.job_application_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CareerNest/job_application_management/JobApplicationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CareerNest.job_application_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
